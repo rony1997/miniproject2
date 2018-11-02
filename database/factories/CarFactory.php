13289cff0230b19d8f3c7c7c99e59080c1453377
rony1997 <rad59@njit.edu>
2018-11-01 23:56:06 -0400

    Task: Car Factory updated

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -1,6 +1,20 @@
+</*?php
+//faker for car database
+use Faker\Generator as Faker;
+
+$factory->define(App\Car::class, function (Faker $faker) {
+    $random=mt_rand(0,2);
+    $CarsArray = array("Ford", "Honda", "Toyota");
+    return [
+        'make' => $CarsArray[$random],
+        'model' => $faker->company,
+        'year' => $faker->year,
+    ];
+});*/
 <?php
 //faker for car database
 use Faker\Generator as Faker;
+
 $factory->define(App\Car::class, function (Faker $faker) {
     $random=mt_rand(0,2);
     $CarsArray = array("Ford", "Honda", "Toyota");

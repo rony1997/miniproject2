b5feb4d7125b09095f39b613f98a0cb1c0843a93
rony1997 <rad59@njit.edu>
2018-11-02 00:01:06 -0400

    Task: about page created

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,6 +3,5 @@
 @section('content')
     <br><br><br>
     <h1>This is my about page</h1>
-    <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a
-        mostly barebones HTML document.</p>
+    <p class="lead">Thank you for visiting.</p>
 @endsection
